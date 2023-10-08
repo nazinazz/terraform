e56550b1300d00bce29b6778be8f594e75d8d804
Ubuntu <ubuntu@ip-172-31-88-75.ec2.internal>
2023-10-08 16:16:27 +0000

    mandotory

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,6 @@
-/* module "ec2-instance" {
+module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.5.0"
+  name          = "single-instance"
+  instance_type = "t2.micro"
 }
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  name = "single-instance"
-  instance_type          = "t2.micro"
-}
-*/
